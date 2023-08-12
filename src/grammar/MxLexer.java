025877ec07c599e6a38bebb614f3a249cac7fcd3
xunying123 <119675097+xunying123@users.noreply.github.com>
2023-08-13 01:37:08 +0800

    Add files via upload

--- src/grammar/MxLexer.java
+++ src/grammar/MxLexer.java
@@ -1,12 +1,9 @@
-// Generated from MxLexer.g4 by ANTLR 4.13.0
+package src.grammar;// Generated from MxLexer.g4 by ANTLR 4.13.0
 import org.antlr.v4.runtime.Lexer;
 import org.antlr.v4.runtime.CharStream;
-import org.antlr.v4.runtime.Token;
-import org.antlr.v4.runtime.TokenStream;
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.atn.*;
 import org.antlr.v4.runtime.dfa.DFA;
-import org.antlr.v4.runtime.misc.*;
 
 @SuppressWarnings({"all", "warnings", "unchecked", "unused", "cast", "CheckReturnValue"})
 public class MxLexer extends Lexer {
@@ -111,7 +108,7 @@ public class MxLexer extends Lexer {
 	}
 
 	@Override
-	public String getGrammarFileName() { return "MxLexer.g4"; }
+	public String getGrammarFileName() { return "src/grammar/MxLexer.g4"; }
 
 	@Override
 	public String[] getRuleNames() { return ruleNames; }
