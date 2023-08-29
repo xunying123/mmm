ae948a1c02bcc9004530b07aca6871d5ec8c2e0d
xunying123 <119675097+xunying123@users.noreply.github.com>
2023-08-29 20:38:39 +0800

    Add files via upload

--- src/Compiler.java
+++ src/Compiler.java
@@ -20,43 +20,44 @@ import java.io.FileOutputStream;
 
 public class Compiler {
     public static void main(String[] args) throws IOException {
+        System.out.println("Generate assembly code");
+        CharStream input = CharStreams.fromStream(System.in);
+        MxLexer lexer = new MxLexer(input);
+        lexer.removeErrorListeners();
+        lexer.addErrorListener(new MxErrorListener());
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        MxParser parser = new MxParser(tokens);
+        parser.removeErrorListeners();
+        parser.addErrorListener(new MxErrorListener());
+        ParseTree parseTreeRoot = parser.fileAnalyze();
+        AstBuilder astbuilder = new AstBuilder();
+        FileAnalyze fileA = (FileAnalyze) astbuilder.visit(parseTreeRoot);
+        GlobalScope globalScope = new GlobalScope();
 
 
+           /* FileOutputStream irOut = new FileOutputStream("output.ll");
+            irOut.write(irFile.toString().getBytes());
+            irOut.close();*/
 
-
+            /*FileOutputStream out = new FileOutputStream("output.s");
+            out.write(asmFile.toString().getBytes());
+            out.close();*/
         if (args.length == 0) {
             System.out.println("Usage: java Compiler <filename>");
             return;
         } else if (args[0].equals("-fsyntax-only")) {
             System.out.println("Semantic check");
-            return;
-        } else if (args[0].equals("-S")) {
-            System.out.println("Generate assembly code");
-            CharStream input = CharStreams.fromStream(System.in);
-            MxLexer lexer = new MxLexer(input);
-            lexer.removeErrorListeners();
-            lexer.addErrorListener(new MxErrorListener());
-            CommonTokenStream tokens = new CommonTokenStream(lexer);
-            MxParser parser = new MxParser(tokens);
-            parser.removeErrorListeners();
-            parser.addErrorListener(new MxErrorListener());
-            ParseTree parseTreeRoot = parser.fileAnalyze();
-            AstBuilder astbuilder = new AstBuilder();
-            FileAnalyze fileA = (FileAnalyze) astbuilder.visit(parseTreeRoot);
-            GlobalScope globalScope = new GlobalScope();
             new SymbolCollector(globalScope).visit(fileA);
             new Checker(globalScope).visit(fileA);
+
+            return;
+        } else if (args[0].equals("-S")) {
             IRFileAnalyze irFile = new IRFileAnalyze();
             new IRBuilder(irFile,globalScope).visit(fileA);
-            FileOutputStream irOut = new FileOutputStream("output.ll");
-            irOut.write(irFile.toString().getBytes());
-            irOut.close();
             AsmFile asmFile = new AsmFile();
             new InstSelector(asmFile).visit(irFile);
             new RegAllocator(asmFile).work();
-            FileOutputStream out = new FileOutputStream("output.s");
-            out.write(asmFile.toString().getBytes());
-            out.close();
+            System.out.print(asmFile.toString());
             return;
         } else {
             System.out.println("Unknown option");
